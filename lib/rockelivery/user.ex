e136b03ba4eba66b8a95e48de6b050fa2e3aeebc
Eliel <elielmichel@gmail.com>
2021-12-05 14:18:26 -0300

    chore: add validations and password hashing

--- lib/rockelivery/user.ex
+++ lib/rockelivery/user.ex
@@ -1,10 +1,13 @@
 defmodule Rockelivery.User do
   use Ecto.Schema
+
   import Ecto.Changeset
+
+  alias Ecto.Changeset
 
   @primary_key {:id, :binary_id, autogenerate: true}
 
-  @required_params [:age, :address, :cep, :cpf, :email, :password_hash, :name]
+  @required_params [:age, :address, :cep, :cpf, :email, :password, :name]
 
   schema "users" do
     field :address, :string
@@ -12,6 +15,7 @@ defmodule Rockelivery.User do
     field :cep, :string
     field :cpf, :string
     field :email, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
     field :name, :string
 
@@ -21,7 +25,24 @@ defmodule Rockelivery.User do
   def changeset(params) do
     %__MODULE__{}
     |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:password_hash, min: 6)
+    |> validate_length(:cep, is: 8)
+    |> validate_length(:cpf, is: 11)
+    |> validate_format(:email, ~r/@/)
+    |> validate_number(:age, greater_than_or_equal_to: 18)
+    |> unique_constraint([:email])
+    |> unique_constraint([:cpf])
+    |> put_password_hash()
 
     # Faz a validação dos campos baseado nos parametros recebidos
   end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    changeset = change(changeset, Pbkdf2.add_hash(password))
+
+    IO.inspect(changeset)
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Rockelivery.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:pbkdf2_elixir, "~> 1.3"}
     ]
   end
 
