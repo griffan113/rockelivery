9891013643d4756571d7539bb8ea2008c9193a51
Eliel <elielmichel@gmail.com>
2021-12-06 21:18:52 -0300

    chore: create fallback controller

--- lib/rockelivery_web/controllers/users_controller.ex
+++ lib/rockelivery_web/controllers/users_controller.ex
@@ -1,10 +1,15 @@
 defmodule RockeliveryWeb.UsersController do
+  import Rockelivery
+
   use RockeliveryWeb, :controller
 
   alias Rockelivery.User
+  alias RockeliveryWeb.FallbackController
+
+  action_fallback FallbackController
 
   def create(conn, params) do
-    with {:ok, %User{} = user} <- Rockelivery.create_user(params) do
+    with {:ok, %User{} = user} <- create_user(params) do
       conn
       |> put_status(:created)
       |> render("create.json", user: user)
