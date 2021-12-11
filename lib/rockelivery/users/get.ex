3991e7c1dfd8690d24b34e08ff870b6e92c0ab73
Eliel <elielmichel@gmail.com>
2021-12-11 14:00:56 -0300

    chore: add uuid verification

--- lib/rockelivery/users/get.ex
+++ lib/rockelivery/users/get.ex
@@ -1,7 +1,15 @@
 defmodule Rockelivery.Users.Get do
+  alias Ecto.UUID
   alias Rockelivery.{Repo, User}
 
   def by_id(id) do
+    case UUID.cast(id) do
+      :error -> {:error, %{status: :bad_request, result: "Id must be an UUID"}}
+      {:ok, uuid} -> get(uuid)
+    end
+  end
+
+  defp get(id) do
     case Repo.get(User, id) do
       nil -> {:error, %{status: :not_found, result: "User not found"}}
       user -> {:ok, user}
