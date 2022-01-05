0e3cc2f3a7f0a0e50c989735dc129fe05d0e94fa
Eliel <elielmichel@gmail.com>
2022-01-05 19:20:20 -0300

    chore: create schemas relations

---
+++ lib/rockelivery/order.ex
@@ -0,0 +1,39 @@
+defmodule Rockelivery.Order do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  alias Rockelivery.{Item, User}
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:address, :comments, :payment_method, :user_id]
+
+  @payment_method_enum [:money, :credit_card, :debit_card]
+
+  # Igual class-validator
+  @derive {Jason.Encoder, only: @required_params ++ [:id]}
+
+  schema "orders" do
+    field :payment_method, Ecto.Enum, values: @payment_method_enum
+    field :address, :string
+    field :comments, :string
+
+    many_to_many :items, Item, join_through: "orders_items"
+    belongs_to :user, User
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params, items) do
+    struct
+    |> cast(params, @required_params)
+    |> put_assoc(:items, items)
+    |> validate_required(@required_params)
+    |> validate_length(:address, min: 10)
+    |> validate_number(:comments, min: 6)
+
+    # put_assoc -> Os items já existem, eles já estão pre-configurados |> Relaciona os items já cadastrados com o pedido
+    # Faz a validação dos campos baseado nos parametros recebidos
+  end
+end
