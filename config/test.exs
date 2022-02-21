d0c259dff953b0b0fca1ba5ff8175baf8182424b
Eliel <70238970+griffan113@users.noreply.github.com>
2022-02-21 10:01:32 -0300

    ci: create environment module to get db password

--- config/test.exs
+++ config/test.exs
@@ -1,3 +1,13 @@
+defmodule Environment do
+  def get_db_password() do
+    if System.get_env("GITHUB_ACTIONS") do
+      "postgres"
+    else
+      "docker"
+    end
+  end
+end
+
 use Mix.Config
 
 # Configure your database
@@ -7,17 +17,10 @@ use Mix.Config
 # Run `mix help test` for more information.
 config :rockelivery, Rockelivery.Repo,
   username: "postgres",
-  password: "docker",
+  password: Environment.get_db_password(),
   database: "rockelivery_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
-
-# Configure the database for GitHub Actions
-if System.get_env("GITHUB_ACTIONS") do
-  config :rockelivery, Rockelivery.Repo,
-    username: "postgres",
-    password: "postgres",
-end
 
 config :rockelivery, Rockelivery.Users.Create, via_cep_adapter: Rockelivery.ViaCep.ClientMock
 
